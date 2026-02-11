141833d5b0cf659bbe3e92d7f813cee3ee7e37ff
lportuano <luchoportuano460@gmail.com>
2026-02-11 18:12:18 -0500

    Carrusel-Roy

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Album } from "../../shared/album/album";
 import { Planes } from "../../shared/planes/planes";
+import { Carrucel } from "../../shared/carrucel/carrucel";
 
 @Component({
   selector: 'app-home',
-  imports: [Album, Planes],
+  imports: [Album, Planes, Carrucel],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/shared/carrucel/carrucel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrucel',
+  imports: [],
+  templateUrl: './carrucel.html',
+  styleUrl: './carrucel.css',
+})
+export class Carrucel {
+
+}
