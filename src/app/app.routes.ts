80466c103d8de7c454861017db588a4ad22313d9
ramijos <amijosroy9@gmail.com>
2026-02-13 08:02:24 -0500

    404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './features/home/home';
 import { Registro } from './features/registro/registro';
 import { Login } from './features/login/login';
 import { Conocenos } from './features/conocenos/conocenos';
+import { Error } from './shared/error/error';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'conocenos', component: Conocenos },
     { path: 'registro', component: Registro },
     { path: 'login', component: Login },
+    { path: 'error', component: Error },
 ];
---
+++ src/app/features/ayuda/ayuda.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Error } from "../../shared/error/error";
+
+@Component({
+  selector: 'app-ayuda',
+  imports: [Error],
+  templateUrl: './ayuda.html',
+  styleUrl: './ayuda.css',
+})
+export class Ayuda {
+
+}
